8a2beab78415cfb3a41ab50854dfc096a37228e5
MatiasRaulIbarra <matiasibarra96@hotmail.com>
2025-09-29 20:48:10 -0300

    feat: Add table User with its properties

---
+++ BlogVerseDB/BlogVerseDB.sql
@@ -0,0 +1,10 @@
+﻿CREATE DATABASE BlogVerseDB;
+
+CREATE TABLE Users(Id  INT NOT NULL IDENTITY ,
+                   Username NVARCHAR(50),
+                   Email NVARCHAR(100),
+                   PassWordHash NVARCHAR(255),
+                   Bio NVARCHAR(500),
+                   CreatedAt DATETIME2,
+                   UpdatedAt DATETIME2,
+                   IsActive BIT);
